6836676d6fabd5d7c5a9e95bab1aa52247e46827
Karuna <revurikarna@gmail.com>
2018-04-20 00:27:43 +0530

    Implemented spring boot web sockets

---
+++ src/main/java/com/att/web/sockets/model/Greeting.java
@@ -0,0 +1,18 @@
+package com.att.web.sockets.model;
+
+public class Greeting {
+
+	private String content;
+
+	public Greeting() {
+	}
+
+	public Greeting(String content) {
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
